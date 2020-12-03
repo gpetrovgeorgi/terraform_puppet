fe026f6c06a50395b30dc6372f3b297c6984fca2
terraform <gpetrov.georgi@gmail.com>
2020-12-03 19:18:49 +0200

    adding a php page

--- code/environments/production/modules/profile/manifests/basic_services.pp
+++ code/environments/production/modules/profile/manifests/basic_services.pp
@@ -1,8 +1,6 @@
 class profile::basic_services (
   Hash $basic_services,
   ){
-#  include phpfpm
-
   exec { 'install_nginx':
     command => 'amazon-linux-extras install nginx1',
     path    => ['/usr/bin', '/usr/sbin'],
@@ -10,10 +8,29 @@ class profile::basic_services (
   }
   service { $basic_services['service_nginx']:
     ensure => $basic_services['nginx_ensure'],
-    enable => $basic_services['nginx_enabled'], 
-  } 
-  package { $basic_services['package_phpfpm']:  
-    ensure  => $basic_services['phpfpm_ensure'], 
+    enable => $basic_services['nginx_enabled'],
+  }
+  package { $basic_services['package_phpfpm']:
+    ensure  => $basic_services['phpfpm_ensure'],
     require => Service[$basic_services['service_nginx']],
+  }
+  service { $basic_services['service_phpfpm']:
+    ensure  => $basic_services['phpfpm_ensure'],
+    enable  => $basic_services['phpfpm_enabled'],
+    require => $basic_services['package_phpfpm'],
+  }
+  file { '/var/www/html':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  file {'/var/www/html/index.php':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('profile/index.php.erb'),
+    require => File['/var/www/html'],
   }
 }
