20dcd51ec183a94255d2de5a507c211c718a605b
terraform <gpetrov.georgi@gmail.com>
2020-12-07 20:22:39 +0200

    adding phpfpm service and facts

--- code/environments/production/modules/profile/manifests/basic_services.pp
+++ code/environments/production/modules/profile/manifests/basic_services.pp
@@ -1,5 +1,12 @@
 class profile::basic_services (
   Hash $basic_services,
+  String $address_ipv4 = $::facts['networking']['interfaces']['eth0']['ip'],
+  String $public_ipv4  = $::facts['ec2_metadata']['public-ipv4'],
+  String $os_type      = $::facts['os']['name'],
+  String $os_release   = $::facts['os']['release']['full'],
+  String $num_cpus     = $::facts['processors']['count'],
+  String $cpu_model    = $::facts['processors']['models'][0],
+  String $mem_total    = $::facts['memory']['system']['total'],
   ){
   exec { 'install_nginx':
     command => 'amazon-linux-extras install nginx1',
@@ -11,8 +18,13 @@ class profile::basic_services (
     enable => $basic_services['nginx_enabled'],
   }
   package { $basic_services['package_phpfpm']:
-    ensure  => $basic_services['phpfpm_ensure'],
+    ensure  => $basic_services['phpfpm_packet_ensure'],
     require => Service[$basic_services['service_nginx']],
+  }
+  service { $basic_services['service_phpfpm']:
+    ensure  => $basic_services['phpfpm_service_ensure'],
+    enable  => $basic_services['phpfpm_enabled'],
+    require => $basic_services['package_phpfpm'],
   }
   file { '/var/www/html':
     ensure => directory,
