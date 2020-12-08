9d3fa61cdf7b0604bf44739f79b19e88f31fd63a
terraform <gpetrov.georgi@gmail.com>
2020-12-08 20:50:01 +0200

    adding nginx default.conf

--- code/environments/production/modules/profile/manifests/basic_services.pp
+++ code/environments/production/modules/profile/manifests/basic_services.pp
@@ -5,13 +5,21 @@ class profile::basic_services (
   String $os_type      = $::facts['os']['name'],
   String $os_release   = $::facts['os']['release']['full'],
   Integer $num_cpus    = $::facts['processors']['count'],
-  Tuple $cpu_model     = $::facts['processors']['models'],
+  String $cpu_model    = $::facts['processors']['models'][0],
   String $mem_total    = $::facts['memory']['system']['total'],
   ){
   exec { 'install_nginx':
     command => 'amazon-linux-extras install nginx1',
     path    => ['/usr/bin', '/usr/sbin'],
     user    => 'root',
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/profile/nginx_default.conf',
+    require => Exec['install_nginx'],
   }
   service { $basic_services['service_nginx']:
     ensure => $basic_services['nginx_ensure'],
@@ -39,5 +47,17 @@ class profile::basic_services (
     mode    => '0755',
     content => template('profile/index.php.erb'),
     require => File['/var/www/html'],
+  }
+  file_line {'www_line1':
+    ensure => present,
+    path   => '/etc/php-fpm.d/www.conf',
+    match  => '^user = apache$',
+    line   => 'user = nginx',
+  }
+  file_line {'www_line2':
+    ensure => present,
+    path   => '/etc/php-fpm.d/www.conf',
+    match  => '^group = apache$',
+    line   => 'group = nginx',
   }
 }
